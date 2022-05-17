8a437b827ebaeff646bcb74207dd27b04025fed2
VladimirZaharov <76486478+VladimirZaharov@users.noreply.github.com>
2022-05-17 07:08:09 +0300

    Update settings.py

--- nekidaem/nekidaem/settings.py
+++ nekidaem/nekidaem/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e0pm%huqac@7us436k)-8)l3nk&tvf58vqhb!6=dj&9mhhxz-l'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
